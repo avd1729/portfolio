58396c4d03ed3c2046e2936f9b6e4b8ea76cd7d7
Aravind <aravind19112003@gmail.com>
2025-05-11 16:42:35 +0530

    Add new projects & existing ones

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -3,6 +3,17 @@ import revise_automate from '../../assets/revise_automate.jpg';
 import starry_ai_night from '../../assets/starry_ai_night.jpg';
 import culinary_bot from '../../assets/culinary_bot.jpg';
 import titanic from '../../assets/titanic.jpg';
+import dataweave from '../../assets/dataweave.jpg'
+import huehush from '../../assets/huehush.jpg'
+import qchat from '../../assets/qchat.jpg'
+import coffeemasters from '../../assets/coffeemasters.jpg'
+import rescuetails from '../../assets/rescuetails.jpg'
+import milo from '../../assets/milo.jpg'
+import textify from '../../assets/textify.jpg'
+import vielnet from '../../assets/vielnet.jpg'
+import talk from '../../assets/talk.jpg'
+import loadbalancer from '../../assets/loadbalancer.jpg'
+import cloud from '../../assets/cloud.jpg'
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -11,28 +22,94 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
+          src={milo}
+          link="https://github.com/avd1729/Milo"
+          h3="Milo"
+          p="Movie Booking System"
+        />
+        <ProjectCard
+          src={textify}
+          link="https://github.com/avd1729/Textify"
+          h3="Textify"
+          p="Smart Android Keyboard"
+        />
+        <ProjectCard
           src={revise_automate}
           link="https://github.com/avd1729/Revision-Automate"
           h3="Revise Automate"
-          p="Automation App"
-        />
-         <ProjectCard
-          src={culinary_bot}
-          link="https://github.com/avd1729/CulinaryBot"
-          h3="Culinary Bot"
-          p="LLM Finetuning App"
+          p="DSA Automation Script"
         />
         <ProjectCard
           src={starry_ai_night}
           link="https://github.com/avd1729/Starry-AI-Night"
-          h3="Starry AI night"
-          p="Generative AI App"
+          h3="Starry AI Night"
+          p="Gen AI (CycleGAN)"
         /> 
+        <ProjectCard
+          src={coffeemasters}
+          link="https://github.com/avd1729/Coffee-Masters-Flutter"
+          h3="Coffee Masters"
+          p="Coffee Ordering App"
+        />
+        <ProjectCard
+          src={dataweave}
+          link="https://github.com/avd1729/DataWeave"
+          h3="DataWeave"
+          p="Synthetic data gen"
+        />
+        <ProjectCard
+          src={culinary_bot}
+          link="https://github.com/avd1729/CulinaryBot"
+          h3="Culinary Bot"
+          p="LLM Finetuning"
+        />
+        <ProjectCard
+          src={huehush}
+          link="https://github.com/avd1729/hue-hush"
+          h3="Huehush"
+          p="PyPi package"
+        /> 
+        <ProjectCard
+          src={vielnet}
+          link="https://github.com/avd1729/VeilNet"
+          h3="VeilNet"
+          p="Steganography (GAN)"
+        />
+        <ProjectCard
+          src={loadbalancer}
+          link="https://github.com/avd1729/Load-Balancer"
+          h3="Load Balancer"
+          p="Custom load balancer"
+        />
+        <ProjectCard
+          src={qchat}
+          link="https://github.com/avd1729/Q-Chat"
+          h3="QChat"
+          p="Quantum Chat App"
+        />
+        <ProjectCard
+          src={talk}
+          link="https://github.com/avd1729/Talk-Space"
+          h3="TalkSpace"
+          p="Social Media App"
+        />
         <ProjectCard
           src={titanic}
           link="https://github.com/avd1729/Titanic-fate-prediction-Website"
-          h3="Titanic fate prediction"
-          p="Machine Learning App"
+          h3="Titanic Fate"
+          p="Machine Learning"
+        />
+        <ProjectCard
+          src={rescuetails}
+          link="https://github.com/avd1729/RescueTails"
+          h3="Rescue Tails"
+          p="Pet Adoption App"
+        />
+        <ProjectCard
+          src={cloud}
+          link="https://github.com/avd1729/Cloudburst-Prediction-System"
+          h3="Cloud Burst"
+          p="Deep Learning"
         />
       </div>
     </section>
